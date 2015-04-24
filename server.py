0ed5481a1bdfe340b5da33afd8c7843c895e7948
Zack Scholl <zack.scholl@gmail.com>
2015-04-24 16:10:48 -0400

    Added tracking, fixed up main page

--- server.py
+++ server.py
@@ -7,78 +7,94 @@ from poem import *
 html_template = """
 <html>
  <head>
-  <title>A simple poem</title>
+  <title>A simple poem generator</title>
 	<link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css" rel="stylesheet">
 <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-62257429-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
 <style>
 body {
-	background: #FFF;
-	color: #111;
-	font: 18px Baskerville, "Palatino Linotype", "Times New Roman", Times, serif;
-	text-align: center;
-	}
-	
-	#poem div, h1, h2, p {
-		margin: 0;
-		padding: 0;
-		}
-	
-		#poem { 
-			margin: auto;
-			padding: 20px 0;
-			text-align: left;
-			width: 650px;
-			}
-		
-			#poem h1, h2 {
-				font-weight: normal;
-				text-align: center;
-				}
-				
-			#poem h1 {
-				font-size: 34px;
-				line-height: 1.2;
-				margin-bottom: 10px;
-				}
-			
-			#poem h2 {
-				color: #666;
-				font-size: 18px;
-				font-style: italic;
-				margin-bottom: 30px;
-				}
-				
-			#poem p {
-				line-height: 1.5;
-				margin-bottom: 15px;
-				}
-			
-			/* The magic of selectors begins... */
-			
-			#poem h2:before {
-				content: '- ';
-				}
-			
-			#poem h2:after {
-				content: ' -';
-				}
-			
-			#poem h2 + p:first-letter {
-					float: left;
-					font-size: 38px;
-					line-height: 1;
-					margin: 2px 5px 0 0;
-				}
-				
-			#poem p:first-line {
-				font-variant: small-caps;
-				letter-spacing: 1px;
-				}
-			
-			#poem p:last-child {
-				margin-bottom: 30px;
-				padding-bottom: 30px;
-				}
+background: #FFF;
+color: #111;
+font: 18px Baskerville, "Palatino Linotype", "Times New Roman", Times, serif;
+text-align: center;
+}
+
+#poem div, h1, h2, p {
+margin: 0;
+padding: 0;
+}
+
+#poem { 
+margin: auto;
+padding: 20px 0;
+text-align: left;
+width: 650px;
+}
+
+#poem h1, h2 {
+font-weight: normal;
+text-align: center;
+}
+
+#poem h1 {
+font-size: 34px;
+line-height: 1.2;
+margin-bottom: 10px;
+}
+
+#poem h2 {
+color: #666;
+font-size: 18px;
+font-style: italic;
+margin-bottom: 30px;
+}
+
+#poem p {
+line-height: 1.5;
+margin-bottom: 15px;
+}
+
+/* The magic of selectors begins... */
+
+#poem h2:before {
+content: '- ';
+}
+
+#poem h2:after {
+content: ' -';
+}
+
+#poem h2 + p:first-letter {
+	float: left;
+	font-size: 38px;
+	line-height: 1;
+	margin: 2px 5px 0 0;
+}
+
+#poem p:first-line {
+font-variant: small-caps;
+letter-spacing: 1px;
+}
+
+#poem p:last-child {
+margin-bottom: 30px;
+padding-bottom: 30px;
+}
+
+#footer {
+position: fixed;
+bottom: 0;
+}
+
 </style>
  </head>
  <body>
@@ -96,6 +112,16 @@ body {
 
 
 %s
+
+
+
+
+<footer class="footer">
+	<div class="container">
+		<p class="text-muted">See how this code passed the turing test <a href="http://rpiai.com/2015/01/24/turing-test-passed-using-computer-generated-poetry/">here</a> and <a href="http://motherboard.vice.com/read/the-poem-that-passed-the-turing-test">here</a>. Also check out the <a href="https://github.com/schollz/poetry-generator">source code!</a></p>
+	</div>
+</footer>
+
 
 
  </body>
